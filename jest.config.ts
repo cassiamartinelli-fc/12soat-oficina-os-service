b153535dc51ad2fe1cb80c89eb19bba653c680c8
Cassia Martinelli <martinelli.cassia@gmail.com>
2026-02-16 19:58:19 -0300

    test: corrige config e scripts de teste

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,19 @@ import type { Config } from "jest";
 
 const config: Config = {
   moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: ".",
+  rootDir: "src",
   testRegex: ".*\\.spec\\.ts$",
   transform: {
     "^.+\\.(t|j)s$": "ts-jest",
   },
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/*.spec.ts",
+    "!**/*.interface.ts",
+    "!**/index.ts",
+    "!main.ts",
+  ],
+  coverageDirectory: "../coverage",
   testEnvironment: "node",
 };
 
